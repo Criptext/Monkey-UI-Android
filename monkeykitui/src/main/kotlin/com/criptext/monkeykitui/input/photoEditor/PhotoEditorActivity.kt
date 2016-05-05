5568ccfdc1d2e34d2ddfb55c496fddbac7b982c0
Gabriel Aumala <gabriel@criptext.com>
2016-05-04 19:02:57 -0500

    New class: PhotoEditTask
    This AsyncTask does bitmap processing and writes it to a a file. It improves
    performance for rotation after croping photos.

--- monkeykitui/src/main/kotlin/com/criptext/monkeykitui/input/photoEditor/PhotoEditorActivity.kt
+++ monkeykitui/src/main/kotlin/com/criptext/monkeykitui/input/photoEditor/PhotoEditorActivity.kt
@@ -1,25 +1,18 @@
 package com.criptext.monkeykitui.input.photoEditor
 
 import android.app.Activity
-import android.content.ContentResolver
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Canvas
-import android.graphics.Matrix
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.LayerDrawable
-import android.os.AsyncTask
-import android.support.v4.app.FragmentManager
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
-import android.util.DisplayMetrics
 import android.util.Log
-import android.view.Display
 import android.view.Menu
-import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.animation.Animation
@@ -29,22 +22,25 @@ import android.widget.ImageView
 import com.criptext.monkeykitui.R
 
 import com.soundcloud.android.crop.Crop
-
-import java.io.ByteArrayOutputStream
 import java.io.File
-import java.io.FileNotFoundException
-import java.io.FileOutputStream
-import java.io.IOException
-import java.io.InputStream
+
 
 /**
  * Created by gesuwall on 9/8/15.
  */
 class PhotoEditorActivity : AppCompatActivity() {
+
     private var photo: ImageView? = null
-    private var retainedFragment: RetainEditPhoto? = null
+    private lateinit var retainedFragment: RetainEditPhoto
     private lateinit var tempFile : File
     lateinit var photoFilePath: String
+
+    private var runningTask : PhotoEditTask? = null
+    set(value) {
+        if(field!=null)
+            field?.cancel(true)
+        field = value
+    }
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,7 +57,6 @@ class PhotoEditorActivity : AppCompatActivity() {
 
         //set Photo
         setEditingPhoto()
-        //System.out.println("degs:  " +  retainedFragment.getDegrees());
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 
@@ -79,12 +74,11 @@ class PhotoEditorActivity : AppCompatActivity() {
      */
     private fun setEditingPhoto() {
         val imgpath = bitmapUri
-        //System.out.println("imgpath is: " + imgpath);
         photo!!.setImageURI(null)
         photo!!.setImageURI(imgpath)
         //Rotar imagen de acuerdo al estado anterior
-        if (retainedFragment!!.isRotated) {
-            val rotated = getRotateDrawable(photo!!.drawable, retainedFragment!!.degrees.toFloat())
+        if (retainedFragment.isRotated) {
+            val rotated = getRotateDrawable(photo!!.drawable, retainedFragment.degrees.toFloat())
             photo!!.setImageDrawable(rotated)
         }
 
@@ -118,17 +112,18 @@ class PhotoEditorActivity : AppCompatActivity() {
     private fun retainFragment() {
         // find the retained fragment on activity restarts
         val fm = supportFragmentManager
-        retainedFragment = fm.findFragmentByTag("data") as RetainEditPhoto?
+        val fragment = fm.findFragmentByTag("data") as RetainEditPhoto?
 
         // create the fragment and data the first time
-        if (retainedFragment == null) {
+        if (fragment == null) {
             // add the fragment
             retainedFragment = RetainEditPhoto()
             fm.beginTransaction().add(retainedFragment, "data").commit()
-            retainedFragment!!.isEdited = false
-            retainedFragment!!.degrees = 0
+            retainedFragment.isEdited = false
+            retainedFragment.degrees = 0
             tempFile.delete() // delete temp file since this is the first time
         }
+            else retainedFragment = fragment
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -138,44 +133,52 @@ class PhotoEditorActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
+
+    /**
+     * Realiza la transformacion de rotacion en el archivo, y posteriormente manda el mismo archivo
+     * al Activity que hace Crop.
+     * @param origin Uri del archivo a rotar y recortar
+     */
+    fun cropAfterRotatingBitmapFile(origin: Uri){
+        val rotationTask = PhotoEditTask(origin, tempFile.absolutePath, contentResolver)
+        rotationTask.setOnBitmapProcessedCallback(Runnable {
+                val destination = Uri.fromFile(tempFile)
+                retainedFragment.isEdited = true
+                retainedFragment.degrees = 0
+                Crop.of(destination, destination).start(this@PhotoEditorActivity)
+        })
+        runningTask = rotationTask
+        rotationTask.execute(editedDegrees)
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-
             android.R.id.home -> onBackPressed()
             R.id.action_crop -> {
 
                 val origin = bitmapUri
                 val destination = Uri.fromFile(tempFile)
-                if (retainedFragment!!.isRotated)
-                // Si hay grados tengo que crear el bitmap rotado
-                    exportBitmap(origin, retainedFragment!!.degrees, Runnable {
-                        retainedFragment!!.isEdited = true
-                        retainedFragment!!.degrees = 0
-                        Crop.of(destination, destination).start(this@PhotoEditorActivity)
-                    })
-                else {
+                if (retainedFragment.isRotated) {
+                    // Si hay grados tengo que crear el bitmap rotado
+                    cropAfterRotatingBitmapFile(origin)
+                }else {
                     Crop.of(origin, destination).start(this)
                 }
             }
             R.id.action_rot -> {
                 val bitmapUri = bitmapUri
-                val fromDegs = retainedFragment!!.degrees
+                val fromDegs = retainedFragment.degrees
                 val toDegs = fromDegs - 90
                 rotateImageView(fromDegs, toDegs)
-                retainedFragment!!.degrees = toDegs
+                retainedFragment.degrees = toDegs
             }
-            else -> {
-            }
-        }//onRotateBtnClick(bitmapUri);
-
+        }
         return true
-
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (data != null && resultCode == Activity.RESULT_OK && requestCode == Crop.REQUEST_CROP) {
-            println("CropRes: " + Crop.getOutput(data))
-            retainedFragment!!.isEdited = true //Nueva foto asi que no hay rotacion
+            retainedFragment.isEdited = true //Nueva foto asi que no hay rotacion
             rotateImageView(0, 0, 0)
             setEditingPhoto()
         }
@@ -206,34 +209,9 @@ class PhotoEditorActivity : AppCompatActivity() {
         }
     }
 
-    /**
-     * Rota la imagen y la guarda en el archivo temporal en un nuevo thread. Cuando termina de
-     * editar el bitmap actualiza la view
-     * @param bitmapUri Uri del bitmap a editar
-     * *
-     * @param degs grados a rotar la imagen
-     * *
-     * @param callback Runnable a ejecutar cuando termine de exportarse
-     */
-    private fun exportBitmap(bitmapUri: Uri, degs: Int, callback: Runnable) {
-        val resolver = this.contentResolver
-        val rotateThread = Thread(Runnable {
-            var bmp : Bitmap? = null
-            try {
-                var bmp = getBitmapFromUri(bitmapUri, resolver)
-                if(bmp != null) {
-                    bmp = rotateBitmap(bmp, degs)
-                    saveBitmapToFile(bmp, tempFile)
-                }
-                photo!!.post(callback)
-            } catch (ex: Exception) {
-                ex.printStackTrace()
-            } finally {
-                bmp?.recycle()
-            }
-        })
-
-        rotateThread.run()
+    override fun onStop(){
+        runningTask?.cancel(true)
+        super.onStop()
     }
 
     /**
@@ -259,7 +237,7 @@ class PhotoEditorActivity : AppCompatActivity() {
      */
     protected val bitmapUri: Uri
         get() {
-            if (retainedFragment!!.isEdited)
+            if (retainedFragment.isEdited)
                 return Uri.fromFile(tempFile)
             else
                 return intent.data
@@ -271,68 +249,20 @@ class PhotoEditorActivity : AppCompatActivity() {
      * @param view Boton que recibio el Click
      */
     fun done(view: View) {
-        val intent = Intent()
-        wrapItUp(intent, false)
+        val processTask = PhotoEditTask(bitmapUri, photoFilePath, contentResolver)
+        Log.d("PhotoEditor", "export source: ${bitmapUri.path} dest: $photoFilePath, degs: $editedDegrees")
+        processTask.setOnBitmapProcessedCallback(Runnable {
+            val intent = Intent()
+            Log.d("PhotoEditor", "exported")
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        })
+        runningTask = processTask
+        processTask.execute(editedDegrees)
     }
 
     protected val editedDegrees: Int
-        get() = retainedFragment!!.degrees
-
-    /**
-     * Termina este activity con un resultado OK y regresa al anterior
-     */
-    protected fun wrapItUp(resultIntent: Intent, isEfimero: Boolean) {
-        val resolver = this.contentResolver
-        val bitmapUri = bitmapUri
-        val degs = editedDegrees
-        //final AQuery aq = new AQuery(this);
-        //final File cache = aq.getCachedFile(MainActivity.URL + AccountManager.instance(getApplicationContext()).get_id() + ".png");
-        object : AsyncTask<Void, Void, ByteArrayOutputStream>() {
-            override fun doInBackground(vararg v: Void): ByteArrayOutputStream {
-                var bmp: Bitmap? = null
-                var fOut = ByteArrayOutputStream()
-                try {
-
-                    //edit new avatar bitmap
-                    bmp = getBitmapFromUri(bitmapUri, resolver)
-                    bmp = rotateBitmap(bmp!!, degs)
-
-                    fOut = ByteArrayOutputStream()
-
-                    val destFile = File(photoFilePath)
-                    Log.d("PhotoEditor","dest path: $photoFilePath")
-                    destFile.delete()
-
-                    bmp!!.compress(Bitmap.CompressFormat.PNG, 100, fOut)
-                    destFile.writeBytes(fOut.toByteArray())
-                    fOut.flush()
-
-                    fOut.close()
-
-
-
-
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                } finally {
-                    bmp?.recycle()
-                }
-
-                return fOut
-            }
-
-            override fun onPostExecute(result: ByteArrayOutputStream) {
-
-
-                resultIntent.data = bitmapUri
-                setResult(Activity.RESULT_OK, resultIntent)
-                finish()
-
-            }
-
-        }.execute()
-
-    }
+        get() = retainedFragment.degrees
 
     class RetainEditPhoto : Fragment() {
 
@@ -356,89 +286,6 @@ class PhotoEditorActivity : AppCompatActivity() {
     companion object {
         val titlePhotoEditor = "PhotoEditorActivity.Title"
         val btnTxtPhotoEditor = "PhotoEditorActivity.ButtonText"
-        val convoPhotoEditor = "PhotoEditorActivity.ConversationId"
         val destinationPath = "PhotoEditorActivity.destinationPath"
-
-        /**
-         * Recibe un uri y devuelve el bitmap encontrado
-         * @param uri
-         * *
-         * @param resolver
-         * *
-         * @return
-         * *
-         * @throws FileNotFoundException
-         * *
-         * @throws IOException
-         */
-        @Throws(FileNotFoundException::class, IOException::class)
-        fun getBitmapFromUri(uri: Uri, resolver: ContentResolver): Bitmap? {
-            val THUMBNAIL_SIZE = 640.0
-            var input: InputStream = resolver.openInputStream(uri)
-
-            val onlyBoundsOptions = BitmapFactory.Options()
-            onlyBoundsOptions.inJustDecodeBounds = true
-            onlyBoundsOptions.inDither = true//optional
-            onlyBoundsOptions.inPreferredConfig = Bitmap.Config.ARGB_8888//optional
-            BitmapFactory.decodeStream(input, null, onlyBoundsOptions)
-            input.close()
-            if (onlyBoundsOptions.outWidth == -1 || onlyBoundsOptions.outHeight == -1)
-                return null
-
-            val originalSize = if (onlyBoundsOptions.outHeight > onlyBoundsOptions.outWidth) onlyBoundsOptions.outHeight else onlyBoundsOptions.outWidth
-
-            val ratio = if (originalSize > THUMBNAIL_SIZE) originalSize / THUMBNAIL_SIZE else 1.0
-
-            val bitmapOptions = BitmapFactory.Options()
-            bitmapOptions.inSampleSize = getPowerOfTwoForSampleRatio(ratio)
-            bitmapOptions.inDither = true//optional
-            bitmapOptions.inPreferredConfig = Bitmap.Config.ARGB_8888//optional
-            input = resolver.openInputStream(uri)
-            val bitmap = BitmapFactory.decodeStream(input, null, bitmapOptions)
-            input.close()
-            return bitmap
-        }
-
-        private fun getPowerOfTwoForSampleRatio(ratio: Double): Int {
-            val k = Integer.highestOneBit(Math.floor(ratio).toInt())
-            if (k == 0)
-                return 1
-            else
-                return k
-        }
-
-        /**
-         * Rota un bitmap
-         * @param source bitmap a rotar
-         * *
-         * @param degs grados a rotar
-         * *
-         * @return el bitma rotado grados
-         */
-        protected fun rotateBitmap(source: Bitmap, degs: Int): Bitmap {
-            val matrix = Matrix()
-            matrix.postRotate(degs.toFloat())
-
-            //Bitmap scaledBitmap = Bitmap.createScaledBitmap(source,source.getWidth(),source.getHeight(),true);
-            return Bitmap.createBitmap(source, 0, 0, source.width, source.height, matrix, true)
-        }
-
-        /**
-         * Guarda un bitmap en el archivo temporal
-         * @param source bitmap a guardar
-         * *
-         * @param dest referencia al File donde se guarda
-         */
-        private fun saveBitmapToFile(source: Bitmap, dest: File) {
-            try {
-                val fout = FileOutputStream(dest)
-                source.compress(Bitmap.CompressFormat.PNG, 85, fout)
-                fout.flush()
-                fout.close()
-            } catch (ex: IOException) {
-                ex.printStackTrace()
-            }
-
-        }
     }
 }
