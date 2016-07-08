eeffa854742ae1aedd9a77b30f402889b9ee9229
Gabriel Aumala <gabrielfima@live.com>
2016-07-08 15:42:39 -0500

    Customize text bubbles
    Bubbles for text messages no longer have a "tail".
    MonkeyAdapter has a new attribute "monkeyConfig" that stores all the supported
    customization. This object can be used to change the background color for text
    messages.

---
+++ monkeykitui/src/main/kotlin/com/criptext/monkeykitui/recycler/MonkeyConfig.kt
@@ -0,0 +1,12 @@
+package com.criptext.monkeykitui.recycler
+
+import android.graphics.Color
+
+/**
+ * Created by gesuwall on 7/8/16.
+ */
+
+class MonkeyConfig {
+    var textBubbleIncomingColor: Int? = null
+    var textBubbleOutgoingColor: Int? = null
+}
